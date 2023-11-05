690994c9a9436ce7fcf8f020818d8696ecf7064b
Aditya Kumar <461997adityakumar@gmail.com>
2023-11-05 09:09:10 +0530

    removed pass

--- Week-19/WealthSQL/config/dbConfig.js
+++ Week-19/WealthSQL/config/dbConfig.js
@@ -1,14 +1,14 @@
-module.exports={
-    host:"localhost",
-    user:"root",
-    password:"ANSU1997vivo@2",
-    database:"nodeWealth",
-    dialect:"mysql",
+module.exports = {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "nodeWealth",
+    dialect: "mysql",
 
-    pool:{
-        max:5,
-        min:0,
-        acquire:30000,
-        idle:10000
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
 }
